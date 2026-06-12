ed71c5784019587ac3739d3db03e6d2f2578a8fb
Nhanvatphu-creator <24107818@st.phenikaa-uni.edu.vn>
2026-06-12 22:14:36 +0700

    Replace MediaPipe with Caffe model for face detection
    Refactor face detection to use Caffe model instead of MediaPipe.

--- app/detection/face_detector.py
+++ app/detection/face_detector.py
@@ -1,41 +1,52 @@
 import cv2
-import mediapipe as mp
+import os
 
 
 class FaceDetector:
     def __init__(self):
-        self.mp_face_detection = mp.solutions.face_detection
-        self.face_detection = self.mp_face_detection.FaceDetection(
-            model_selection=0,
-            min_detection_confidence=0.5
+
+        base_dir = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                ".."
+            )
         )
 
-        self.mp_drawing = mp.solutions.drawing_utils
+        prototxt = os.path.join(
+            base_dir,
+            "deploy.prototxt"
+        )
+
+        model = os.path.join(
+            base_dir,
+            "res10_300x300_ssd_iter_140000.caffemodel"
+        )
+
+        print("Prototxt:", prototxt)
+        print("Model:", model)
+
+        print("Prototxt exists:", os.path.exists(prototxt))
+        print("Model exists:", os.path.exists(model))
+
+        self.net = cv2.dnn.readNetFromCaffe(
+            prototxt,
+            model
+        )
 
     def detect_faces(self, frame):
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        results = self.face_detection.process(rgb_frame)
+        h, w = frame.shape[:2]
 
-        if results.detections:
-            for detection in results.detections:
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(frame, (300, 300)),
+            1.0,
+            (300, 300),
+            (104.0, 177.0, 123.0)
+        )
 
-                bbox = detection.location_data.relative_bounding_box
+        self.net.setInput(blob)
 
-                h, w, _ = frame.shape
-
-                x = int(bbox.xmin * w)
-                y = int(bbox.ymin * h)
-                width = int(bbox.width * w)
-                height = int(bbox.height * h)
-
-                cv2.rectangle(
-                    frame,
-                    (x, y),
-                    (x + width, y + height),
-                    (0, 255, 0),
-                    2
-                )
+        detections = self.net.forward()
 
         return frame
-
